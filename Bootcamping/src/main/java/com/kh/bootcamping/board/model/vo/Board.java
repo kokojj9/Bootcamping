37d958234f13866fe89e189424e1bcda334c826b
kokojj9 <kovjscl976@naver.com>
2024-05-13 16:22:10 +0900

    마이페이지 sql작업

--- Bootcamping/src/main/java/com/kh/bootcamping/board/model/vo/Board.java
+++ Bootcamping/src/main/java/com/kh/bootcamping/board/model/vo/Board.java
@@ -1,5 +1,24 @@
 package com.kh.bootcamping.board.model.vo;
 
-public class Board {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Board {
+	
+	private int boardNo;
+	private String boardTitle;
+	private String boardContent;
+	private String createDate;
+	private String updateDate;
+	private String status;
+	private int memberNo;
+	private String categoryName;
+	private int count;
+	
 }
