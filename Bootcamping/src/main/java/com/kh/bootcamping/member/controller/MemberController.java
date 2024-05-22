e7abaf286dd8c8a4ff84c0cf9da9bd90f35bdd42
kokojj <kovjscl976@naver.com>
2024-05-23 00:14:06 +0900

    123

--- Bootcamping/src/main/java/com/kh/bootcamping/member/controller/MemberController.java
+++ Bootcamping/src/main/java/com/kh/bootcamping/member/controller/MemberController.java
@@ -225,42 +225,6 @@ public class MemberController {
 	    response.addCookie(saveIdCookie);
 	}
 	
-	// 로그인 포워딩 메서드
-	@GetMapping("loginForm")
-	public String forwardToLoginPage() {
-		return "member/login";
-	}
-	
-	//회원가입 화면 포워딩 메서드
-	@GetMapping("enrollForm")
-	public String forwardToEnrollForm() {
-		return "member/enrollForm";
-	}
-	
-	//마이페이지 메서드
-	@GetMapping("myPage")
-	public String forwardMyPage(String memberId, Model model) {
-		model.addAttribute("myPageInfo", memberService.searchMyPage(memberId));
-		return "member/myPage";
-	}
-	
-	// 회원 정보 수정 페이지 포워딩 메서드
-	@GetMapping("editForm")
-	public String forwardEditMember(Member member, Model model) {
-		return "member/editForm";
-	}
-
-	// 아이디 찾기 포워딩 메서드
-	@GetMapping("searchIdForm")
-	public String forwardSearchId() {
-		return "member/searchMemberId";
-	}
-	
-	// 비밀번호 찾기 포워딩 메서드
-	@GetMapping("searchPwdForm")
-	public String forwardSearchPwd() {
-		return "member/searchMemberPwd";
-	}
 		
 
 }
