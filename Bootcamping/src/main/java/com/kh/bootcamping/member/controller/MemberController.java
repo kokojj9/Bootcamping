bcdf11fdeedafcf935b4a80fa128408c16c1c2f5
kokojj9 <kovjscl976@naver.com>
2024-05-23 10:02:48 +0900

    rest규칙 적용

--- Bootcamping/src/main/java/com/kh/bootcamping/member/controller/MemberController.java
+++ Bootcamping/src/main/java/com/kh/bootcamping/member/controller/MemberController.java
@@ -194,69 +194,7 @@ public class MemberController {
 		return "member/searchMemberPwd";
 	}
 	
-<<<<<<< Updated upstream
-	/***
-	 * 쿠키 생성 메서드
-	 * @param saveId 사용자가 입력한 아이디
-	 * @param response
-	 */
-	private void saveIdCookie(String saveId, HttpServletResponse response) {
-	    Cookie saveIdCookie = new Cookie("saveId", saveId);
-	    saveIdCookie.setMaxAge(60 * 60 * 24); // 1 day
-	    saveIdCookie.setPath("/"); // 쿠키가 사이트 전역에서 유효하도록 설정
-	    saveIdCookie.setHttpOnly(true); // 클라이언트 측 스크립트에서 쿠키에 접근하지 못하도록 설정
-	    response.addCookie(saveIdCookie);
-	}
 
-	/***
-	 * 쿠키 삭제 메서드
-	 * @param response
-	 */
-	private void deleteIdCookie(HttpServletResponse response) {
-	    Cookie saveIdCookie = new Cookie("saveId", "");
-	    saveIdCookie.setMaxAge(0); // 쿠키 만료
-	    saveIdCookie.setPath("/"); // 경로 설정 일치
-	    response.addCookie(saveIdCookie);
-	}
-	
-	// 로그인 포워딩 메서드
-	@GetMapping("loginForm")
-	public String forwardToLoginPage() {
-		return "member/login";
-	}
-	
-	//회원가입 화면 포워딩 메서드
-	@GetMapping("enrollForm")
-	public String forwardToEnrollForm() {
-		return "member/enrollForm";
-	}
-	
-	//마이페이지 메서드
-	@GetMapping("myPage")
-	public String forwardMyPage(String memberId, Model model) {
-		model.addAttribute("myPageInfo", memberService.searchMyPage(memberId));
-		return "member/myPage";
-	}
-	
-	// 회원 정보 수정 페이지 포워딩 메서드
-	@GetMapping("editForm")
-	public String forwardEditMember(Member member, Model model) {
-		return "member/editForm";
-	}
-	
-	// 아이디 찾기 포워딩 메서드
-	@GetMapping("searchIdForm")
-	public String forwardSearchId() {
-		return "member/searchMemberId";
-	}
-	
-	// 비밀번호 찾기 포워딩 메서드
-	@GetMapping("searchPwdForm")
-	public String forwardSearchPwd() {
-		return "member/searchMemberPwd";
-	}
-=======
->>>>>>> Stashed changes
 		
 	
 	
